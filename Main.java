422da77b24a562e3484090dda544848e55aa44fc
Phillip Blake <68248890+mdpcb@users.noreply.github.com>
2024-11-06 13:08:06 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class Main{
+  private static final String ANSI_YELLOW = "\u001B[33m";
+  private static final String ANSI_RED = "\u001B[31m";
+  private static final String ANSI_BLUE = "\u001B[34m";
+  private static final String ANSI_RESET = "\u001B[0m";
+  
   public static void main(String[] args){
   }
 }
